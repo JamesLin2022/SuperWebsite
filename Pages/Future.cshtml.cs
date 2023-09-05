8ddf14c1882d1b24dd3bde845ccd9e66a0f5cf24
JamesLin2022 <james.09461@gmail.com>
2023-09-05 14:50:07 -0700

    first commit

---
+++ Pages/Future.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperWeb.model;
+
+namespace SuperWeb.Pages;
+
+public class FutureModel : PageModel
+{
+    public decimal MonthlyInvestment { get; set; }
+    public decimal YearlyInterestRate { get; set; }
+    public int Years { get; set; }
+
+    private readonly ILogger<FutureModel> _logger;
+
+    public FutureModel(ILogger<FutureModel> logger)
+    {
+        _logger = logger;
+    }
+
+    
+
+    public void OnGet()
+    {
+        ViewData["FV"] = 0;
+    }
+
+    public void OnPost(FutureValue model)
+        {
+            ViewData["FV"] = model.CalculateFutureValue();
+        }
+}
+
---
+++ Pages/FutureValue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SuperWeb.Pages
+{
+    public class FutureValueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
